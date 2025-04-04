adb9e6cf42e87848a9a7f4d29d660f6c098c96ca
Leonardo Hoffmann <147009433+leohd98@users.noreply.github.com>
2025-04-04 14:55:52 +0000

    aula_04.04

---
+++ src/Exercícios.py
@@ -0,0 +1,45 @@
+#salario_bruto = float(input('Digite o salário bruto: R$'))
+#bonus = salario_bruto + (salario_bruto * 0.2)
+#reajuste = bonus - (bonus * 0.07)
+#print(f'O valor do salário com o bônus é: R${bonus}')
+#print(f'O valor do salário com o desconto é: R${reajuste}')
+
+# ------------------------------------------------------
+
+#nota1 = float(input('Digite a primeira nota: '))
+#nota2 = float(input('Digite a segunda nota: '))
+#nota3 = float(input('Digite a terceira nota: '))
+#soma = nota1 + nota2 + nota3
+#print(f'Notas lidas: {nota1}, {nota2}, {nota3}')
+#print(f'Soma das notas: {soma}')
+#print(f'A média das notas: {(soma / 3):.2f}')
+
+# ------------------------------------------------------
+
+# mensagem = '''
+# Hello
+# wow dasdsasdasd
+# teste
+# '''
+# print(mensagem)
+
+# ------------------------------------------------------
+
+# raio = float(input('Digite o raio: '))
+# perimetro = 2 * 3.14 * raio
+# area = 3.14 * (raio * raio)
+# print(f'Perímetro: {perimetro} | Área: {area}')
+
+# ------------------------------------------------------
+
+# a = input('a: ')
+# b = input('b: ')
+# aux = a
+# a = b
+# b = aux
+# print(f'a: {a}')
+# print(f'b: {b}')
+
+# ------------------------------------------------------
+
+nomes 
